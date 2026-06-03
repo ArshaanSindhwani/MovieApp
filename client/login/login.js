29d7fe8f9f562a63cfa4ad7082a1f0926cb07f53
vikram kapoor <vikkapoor@mail.com>
2026-06-03 12:03:01 +0100

    edit for username only

--- client/login/login.js
+++ client/login/login.js
@@ -1,8 +1,3 @@
-// NOTE:
-// Verify the API URL in response variable matches the target environment (local, staging, production).
-// Users log in using an email address, however the backend currently expects this value in a field named `username`. 
-// Confirm we are happy with this
-
 document.getElementById("login-form").addEventListener("submit", async (e) => {
     e.preventDefault();
 
@@ -15,17 +10,17 @@ document.getElementById("login-form").addEventListener("submit", async (e) => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            username: form.get("email"),  // Notice it will reference username in backend, but accepts email in frontend
+            username: form.get("username"),
             password: form.get("password")
         })
     }
 
-    const response = await fetch("http://localhost:3000/users/login", options); // Link to be editied to match backend
+    const response = await fetch("http://localhost:3000/users/login", options); // Link to be edited to match backend
     const data = await response.json();
 
     if (response.status == 200) {
-        localStorage.setItem("token", data.token);
-        window.location.assign("../homepage/home.html"); 
+        localStorage.setItem("token", data.token); // Delete if backend hasm't been coded for token
+        window.location.assign("../homepage/home.html");  // Ensure file path is correct for homepage
       } else {
         alert(data.error);
       }
