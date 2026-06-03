201fee858cfdc08b8f9422bb9c710840c0a723a7
AbuxKareem <akareembusiness@gmail.com>
2026-06-03 13:34:48 +0100

    connect.js

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const db = new Pool({
-  connectionString: process.env.NODE_ENV === 'test' ? process.env.TEST_DB_URL : process.env.DB_URL
+  connectionString: process.env.DB_URL
 });
 
 module.exports = db;
